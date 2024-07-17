5af3abc1e1cfde56a27f53ffe0f05f5ae016d024
aamir105 <aamiranwer2018@gmail.com>
2024-07-17 12:19:35 +0530

    UC2 Updated

--- src/git/EmployeeWage.java
+++ src/git/EmployeeWage.java
@@ -1,7 +1,23 @@
 package git;
 
 public class EmployeeWage {
-    public static void main(String[] args) {
+    public static void UC2(){
+        int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR=20;
+        int empHrs=0;
+        int empWage=0;
+        double empCheck= Math.floor(Math.random()*10)%2;
+        if (empCheck == IS_FULL_TIME){
+            empHrs=8;
+        }
+        else {
+            empHrs=0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage : "+empWage);
+    }
+
+    public static void UC1(){
         int IS_FULL_TIME = 1;
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_FULL_TIME) {
@@ -9,5 +25,9 @@ public class EmployeeWage {
         } else {
             System.out.println("Employee is Abscent.");
         }
+    }
+    public static void main(String[] args) {
+        UC1();
+        UC2();
     }
 }
