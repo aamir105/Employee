98fd5ff4476967fba954ddc583398af1f4968891
aamir105 <aamiranwer2018@gmail.com>
2024-07-18 12:28:37 +0530

    ADDED: UC3

--- src/git/EmployeeWage.java
+++ src/git/EmployeeWage.java
@@ -26,8 +26,29 @@ public class EmployeeWage {
             System.out.println("Employee is Abscent.");
         }
     }
+    public static void UC3(){
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
+        int EMP_RATE_PER_HRS = 20;
+
+        int empHrs = 0;
+        int empWage = 0;
+
+        double empCheck= Math.floor(Math.random()*10)%3;
+        if (empCheck == IS_PART_TIME){
+            empHrs = 4;
+        } else if (empCheck == IS_FULL_TIME) {
+            empHrs = 8;
+        }
+        else {
+            empHrs = 0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HRS;
+        System.out.println("Employee Wage: "+ empWage);
+    }
     public static void main(String[] args) {
         UC1();
         UC2();
+        UC3();
     }
 }
